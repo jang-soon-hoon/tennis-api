df8532ff05d38e968ff1c801e6c92b434ce6bc3f
DESKTOP-RGJJS5G\jsh <jang4716@gmail.com>
2026-02-19 19:29:16 +0900

    dddddd

--- models/match.go
+++ models/match.go
@@ -4,8 +4,8 @@ import "time"
 
 type Match struct {
 	ID        string    `json:"id" firestore:"-"` // ID는 문서 이름에서 가져오므로 생략
-	PlayerA   string    `json:"playerA" firestore:"playerA"`
-	PlayerB   string    `json:"playerB" firestore:"playerB"`
+	PlayerA   string    `json:"playerA" firestore:"PlayerA"`
+	PlayerB   string    `json:"playerB" firestore:"PlayerB"`
 	ScoreA    int       `json:"scoreA" firestore:"scoreA"`
 	ScoreB    int       `json:"scoreB" firestore:"scoreB"`
 	Status    string    `json:"status" firestore:"status"` // "scheduled", "live", "finished"
